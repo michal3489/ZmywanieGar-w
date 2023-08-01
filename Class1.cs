8f2f50051de02bf7bc685246d5fd7145a510a8a9
michal3489 <michal3489@gmail.com>
2023-08-01 15:43:21 +0200

    Dodaj pliki projektów.

---
+++ Class1.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System;
+namespace Zmywara
+
+public void Main()
+{
+    // Fill the sink with hot water.
+    Sink.FillWithHotWater();
+
+
+    // Add dish soap to the water.
+    Sink.AddDishSoap();
+
+
+    // Place the dishes in the sink.
+    Dishes.PlaceInSink();
+
+
+    // Scrub the dishes with a sponge.
+    Dishes.ScrubWithSponge();
+
+
+    // Rinse the dishes with water.
+    Dishes.RinseWithWater();
+
+
+    // Dry the dishes with a towel.
+    Dishes.DryWithTowel();
+
+
+    // Put the dishes away.
+    Dishes.PutAway();
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Reflection.Metadata;
+
+namespace Zmywara // Note: actual namespace depends on the project name.
+{
+    public class Sink
+    {
+        public void FillWithHotWater()
+        {
+            int water1 = 100;
+            int water2 = 1;
+
+            Console.WriteLine(water1);
+            Console.WriteLine(water2);
+        }
+        public void AddDishSoap()
+        {
+            string soap = "Podaj ile kropel płynu ci potrzeba";
+            int soapLoaded;
+            Console.WriteLine(soap);
+
+            Console.ReadLine(soapLoaded);
+        }
+    }
+
+    public class Dishes
+    {
+        public void PlaceInSink()
+        {
+
+        }
+        public void ScrubWithSponge()
+        {
+
+        }
+        public void RinseWithWater()
+        {
+
+        }
+        public void DryWithTowel()
+        {
+
+        }
+        public void PutAway()
+        {
+
+        }
+    }
+}
